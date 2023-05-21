02ad8e2538a87ed32848f9f2b0336a00679c4e89
Eric Bloch <eric.bloch@gmail.com>
2023-05-21 16:59:41 -0700

    Update index.tsx
    Add a notewize bit

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,11 +13,11 @@ const Home: NextPage = () => {
       {!session ? (
         <div className="row">
           <div className="col-6">
-            <h1 className="header">Supabase Auth + Storage</h1>
+            <h1 className="header">Supabase Auth + Storage Demo for Notewize 🎵 /h1>
             <p className="">
               Experience our Auth and Storage through a simple profile management example. Create a
               user profile and upload an avatar image. Fast, simple, secure.
-            </p>
+              </p>
           </div>
           <div className="col-6 auth-widget">
             <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} theme="dark" />
